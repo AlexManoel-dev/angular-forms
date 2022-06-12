060b1de021b4cbc1e97312f9276684b0c180d999
Alex Manoel <alexmanoel93@gmail.com>
2022-06-12 16:49:57 -0300

    Populando campos com setValue(seta o form inteiro) e patchValue(seta somente os valores desejados) (CEP)

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -41,7 +41,7 @@ export class TemplateFormComponent implements OnInit {
     }
   }
 
-  consultaCEP(cep: any) {
+  consultaCEP(cep: any, form: any) {
     // Nova variável "cep" somente com dígitos.
     cep = cep.replace(/\D/g, '');
 
@@ -52,10 +52,55 @@ export class TemplateFormComponent implements OnInit {
 
       // Valida o formato do CEP.
       if(validaCep.test(cep)){
+        // O método resetaDadosForm, vai mudar qualquer informação que esteja dentro do input e colocar a que vem do via cep(caso o usuário tenha colocado na mão e depois colocado o cep, assim chamando o serviço do viaCEP)
+        this.resetaDadosForm(form);
         this.http.get(`//viacep.com.br/ws/${cep}/json`)
           .pipe(map((dados: any) => dados))
-          .subscribe(dados => console.log(dados));
+          .subscribe(dados => this.populaDadosForm(dados, form));
         }
       }
+    }
+
+    populaDadosForm(dados: any, formulario: any) {
+      // Com os values do nome e email, eles não são resetados, e exibem o valor já existente
+      // O setValue seta todos os valores, portanto é necessário acessar até mesmo os que não se deseja alterar
+      // formulario.setValue({
+      //   nome: formulario.value.nome,
+      //   email: formulario.value.email,
+      //   endereco: {
+      //     rua: dados.logradouro,
+      //     cep: dados.cep,
+      //     numero: '',
+      //     complemento: dados.complemento,
+      //     bairro: dados.bairro,
+      //     cidade: dados.localidade,
+      //     estado: dados.uf
+      //   }
+      // });
+      
+      // O patchValue vai setar somente os campos que forem especificados dentro dele
+      formulario.form.patchValue({
+        endereco: {
+          rua: dados.logradouro,
+          complemento: dados.complemento,
+          bairro: dados.bairro,
+          cidade: dados.localidade,
+          estado: dados.uf
+        }
+      });
+      
+      //console.log(form);
+    }
+    
+    resetaDadosForm(formulario: any) {
+      formulario.form.patchValue({
+        endereco: {
+          rua: null,
+          complemento: null,
+          bairro: null,
+          cidade: null,
+          estado: null
+        }
+      });
     }
   }
